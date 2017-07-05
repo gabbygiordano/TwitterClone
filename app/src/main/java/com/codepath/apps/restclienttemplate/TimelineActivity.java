9ded7a84326a48815615f5b9d57f4ae58c9d2bbb
Gabby Giordano <gabby.giordano@gmail.com>
2017-07-05 11:51:50 -0700

    updated to include tab layout

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -2,7 +2,9 @@ package com.codepath.apps.restclienttemplate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
@@ -12,6 +14,7 @@ import android.widget.ProgressBar;
 
 import com.codepath.apps.restclienttemplate.fragments.HomeTimelineFragment;
 import com.codepath.apps.restclienttemplate.fragments.TweetsListFragment;
+import com.codepath.apps.restclienttemplate.fragments.TweetsPagerAdapter;
 
 public class TimelineActivity extends AppCompatActivity {
 
@@ -21,30 +24,39 @@ public class TimelineActivity extends AppCompatActivity {
     private Button replyButton;
 
     TweetsListFragment fragmentTweetsList;
+
     HomeTimelineFragment fragmentHomeTimeline;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timeline);
-        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        // swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
         // Setup refresh listener which triggers new data loading
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
+        // swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+        //    @Override
+        //    public void onRefresh() {
                 // Your code to refresh the list here.
                 // Make sure you call swipeContainer.setRefreshing(false)
                 // once the network request has completed successfully.
-                fragmentHomeTimeline.fetchTimelineAsync(0);
-            }
-        });
+        //        fragmentHomeTimeline.fetchTimelineAsync(0);
+        //    }
+        // });
         // Configure the refreshing colors
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
-                android.R.color.holo_green_light,
-                android.R.color.holo_orange_light,
-                android.R.color.holo_red_light);
+        //swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+        //        android.R.color.holo_green_light,
+        //        android.R.color.holo_orange_light,
+        //        android.R.color.holo_red_light);
 
-        fragmentTweetsList = (TweetsListFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_timeline);
+        // get the view pager
+        ViewPager vpPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // set the adapter forthe pager
+        vpPager.setAdapter(new TweetsPagerAdapter(getSupportFragmentManager(), this));
+
+        // setup the tablayout to use the view pager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(vpPager);
     }
 
 
@@ -78,6 +90,7 @@ public class TimelineActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        TweetsListFragment fragmentTweetsList = new TweetsListFragment();
         fragmentTweetsList.onFinish(requestCode, resultCode, data);
     }
 }
